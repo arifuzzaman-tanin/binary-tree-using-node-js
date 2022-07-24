84574bd54afe8c8f673d3247f27479e9b544bfaa
arifuzzaman-tanin <arifuzzaman.tanin@outlook.com>
2022-07-24 11:32:30 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const node = new Node(value);
+        if (this.root == null) {
+            this.root = node;
+        } else {
+            let currentNode = this.root;
+            while (true) {
+                //Left
+                if (currentNode.value > value) {
+                    if (!currentNode.left) {
+                        currentNode.left = node;
+                        return this;
+                    }
+                    currentNode = currentNode.left;
+                } else {
+                    //Right
+                    if (currentNode.value < value) {
+                        if (!currentNode.right) {
+                            currentNode.right = node;
+                            return this;
+                        }
+                        currentNode = currentNode.right;
+                    }
+                }
+            }
+        }
+    }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+console.log(JSON.stringify(tree.root));
+
+function traverse(node) {
+    const tree = {value: node.value};
+    tree.left = node.left === null ? null : traverse(node.left);
+    tree.right = node.right === null ? null : traverse(node.right);
+    return tree;
+}
